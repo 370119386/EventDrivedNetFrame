bb9b5e0ad3762a84643f88b26e8304bde186cc1d
shenshaojun <370119386@qq.com>
2019-09-18 12:54:51 +0800

    //网络模块接口更新

--- EventDrivedNetFrame/DDNFCommon/DDNFNetPlugin/INetModule.cc
+++ EventDrivedNetFrame/DDNFCommon/DDNFNetPlugin/INetModule.cc
@@ -1,2 +1,13 @@
 #include "INetModule.h"
 
+using namespace DDNF;
+
+DDNF::INetModule::INetModule(IPluginManager* p)
+{
+	m_pPluginManager = p;
+}
+
+DDNF::INetModule::~INetModule()
+{
+	m_pPluginManager = NULL;
+}
--- EventDrivedNetFrame/DDNFCommon/DDNFNetPlugin/INetModule.h
+++ EventDrivedNetFrame/DDNFCommon/DDNFNetPlugin/INetModule.h
@@ -1,10 +1,40 @@
 #ifndef __I__NET__MODULE__H__
 #define __I__NET__MODULE__H__
 #include "DDNFCommon/DDNFPluginModule/IModule.h"
+#include "DDNFCommon/DDNFPluginModule/IPluginManager.h"
+
 namespace DDNF
 {
 	class INetModule : public IModule
 	{
+	public:
+		INetModule(IPluginManager* p);
+		virtual ~INetModule();
+
+		template<typename BaseType>
+		bool AddReceiveCallBack(const int nMsgID, BaseType* pBase, void (BaseType::* handleRecieve)(const NFSOCK, const int, const char*, const uint32_t))
+		{
+			NET_MESSAGE_HANDLER_FUNCTOR functor = std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
+			NET_MESSAGE_HANDLER_FUNCTOR_PTR functorPtr(new NET_MESSAGE_HANDLER_FUNCTOR(functor));
+
+			return AddReceiveCallBack(nMsgID, functorPtr);
+		}
+
+		template<typename BaseType>
+		bool AddReceiveCallBack(BaseType* pBase, void (BaseType::* handleRecieve)(const NFSOCK, const int, const char*, const uint32_t))
+		{
+			NET_MESSAGE_HANDLER_FUNCTOR functor = std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
+			NET_MESSAGE_HANDLER_FUNCTOR_PTR functorPtr(new NET_MESSAGE_HANDLER_FUNCTOR(functor));
+
+			return AddReceiveCallBack(functorPtr);
+		}
+
+		virtual bool AddReceiveCallBack(const int nMsgID, const NET_MESSAGE_HANDLER_FUNCTOR_PTR& cb) = 0;
+		virtual bool AddReceiveCallBack(const NET_MESSAGE_HANDLER_FUNCTOR_PTR& cb) = 0;
+
+	private:
+		std::list<NET_MESSAGE_HANDLER_FUNCTOR_PTR> mNetMsgHandlers;
+		std::map<int, std::list<NET_MESSAGE_HANDLER_FUNCTOR_PTR>> mxReceiveCallBack;
 
 	};
 }
