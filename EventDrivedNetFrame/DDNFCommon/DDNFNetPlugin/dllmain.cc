2675081bb1573fe25bc75014cfdadf26c0d1bdf4
shenshaojun <370119386@qq.com>
2019-09-20 12:50:12 +0800

    //提交windows x64 libprotobuf_d.lib

--- EventDrivedNetFrame/DDNFCommon/DDNFNetPlugin/dllmain.cc
+++ EventDrivedNetFrame/DDNFCommon/DDNFNetPlugin/dllmain.cc
@@ -6,7 +6,7 @@
 #pragma comment( lib, "ws2_32" )
 //#pragma comment( lib, "NFCore_d.lib" )
 //#pragma comment( lib, "NFMessageDefine_d.lib" )
-//#pragma comment( lib, "libprotobuf_d.lib" )
+#pragma comment( lib, "libprotobuf_d.lib" )
 #pragma comment( lib, "libevent_core.lib" )
 
 #elif DDNF_PLATFORM == DDNF_PLATFORM_LINUX || DDNF_PLATFORM == DDNF_PLATFORM_ANDROID
